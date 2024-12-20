0ad4e550f7e70f797a1845a784df04bfead2cd35
Zhi Heng <tanzhiheng26@gmail.com>
2024-12-20 19:38:34 +0800

    Complete day 17 part 2

--- day17/solution.py
+++ day17/solution.py
@@ -67,4 +67,41 @@ def part1(file):
     output = execute_program(a, b, c, program)
     return ','.join(list(map(str, output)))
 
+"""
+Part 2
+------
+Program: 2,4,1,1,7,5,1,5,4,5,0,3,5,5,3,0
+
+bst(4): b = a % 8. Last 3 bits of a.
+bxl(1): b = b ^ 1
+cdv(5): c = a // (2 ** b)
+bxl(5): b = b ^ 5
+bxc(5): b = b ^ c
+adv(3): a = a // 8. Right shift a by 3.
+out(5): output b % 8
+jnz(0): jump to 1st instruction if a != 0
+"""
+# Based on https://www.reddit.com/r/adventofcode/comments/1hgo81r/2024_day_17_genuinely_enjoyed_this/
+def part2(file):
+    _, b, c, program = read_input(file)
+    a_values = []
+    no_jump = program[:-2]
+
+    def dfs(a, i):
+        if int(execute_program(a, b, c, no_jump)[0]) != program[i]:
+            return 
+        if i == 0:
+            a_values.append(a)
+            return 
+
+        for next_3_bits in range(8):
+            dfs(a * 8 + next_3_bits, i - 1)
+    
+    # a cannot be 0 at the start of the last iteration
+    for a in range(1, 8):
+        dfs(a, len(program) - 1)
+    
+    return min(a_values)
+
 print(part1("input.txt"))
+print(part2("input.txt"))
